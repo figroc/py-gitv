fd26d6f26c7dc3ce0e699fb56d7cea8f695de589
P Chen <figroc@gmail.com>
2024-09-01 14:13:58 +0800

    rename version separator pattern

--- gitv/plugins/setup.py
+++ gitv/plugins/setup.py
@@ -3,11 +3,11 @@ from setuptools import Distribution
 from .. import build_version
 
 _GITV = __name__.split(".")[0]
-_P = re.compile(r"([-.]?[a-z]+)", re.I)
+_VSEP = re.compile(r"([-.]?[a-z]+)", re.I)
 
 
 def configure(dist: Distribution):
     if _GITV in dist.setup_requires:
         if v := dist.metadata.version:
-            v = _P.split(v, 1)[0]
+            v = _VSEP.split(v, 1)[0]
         dist.metadata.version = build_version(v)
